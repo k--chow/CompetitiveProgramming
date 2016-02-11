9bf77f831621f4762c31a82881ce2ad0bef4fc0f
kchow23 <kchow23@ufl.edu>
2016-02-10 20:31:46 -0500

    ACODE

--- SPOJ/ACODE.cpp
+++ SPOJ/ACODE.cpp
@@ -12,44 +12,64 @@ using namespace std;
 int main()
 {
     string i;
-    cin >> i;
+    vector<long long> ans;
+    while (1)
+    {
+        cin >> i;
+        if (i == "0")
+        {
+            break;
+        }
+
     vector<long long> arr;
-    arr.push_back(1);
+    arr.push_back(0);
     arr.push_back(1);
     //100 fibonacci
-    for(int t=0; t<30; t++)
-    {
-        arr.push_back(arr.at(t) + arr.at(t+1));
-    }/*
+
     for(int t=1; t<i.length()+1; t++)//each substring
     {
         string c = i.substr(0, t);
-        cout << c << endl;
-        int total = arr.back();
-        int a = c.length()-1;
-        for(int j=0; j<c.length()-1; j++)//each pair
+        //cout << c << endl;
+        int total = 0;
+        //int a = c.length()-1;
+        for(int j=t-2; j<c.length()-1; j++)//each pair
         {
             //check if pair is < 27
             string ch = i.substr(j, 2);
             int check = atoi(i.substr(j, 2).c_str());
-            cout << "check " << check << endl;
+            //cout << "check " << check << endl;
             //total = j;
             if (check > 26)
             {
-                a--;
+                total--;
+            }
+            if (ch == "00")
+            {
+                //make it all zero
+            }
+
+            if (ch[0] == '0' || ch[1] == '0')
+            {
+                //subtract from previous
+                arr.at(t-1)--;
             }
 
 
+
         }
-        total+=a;
+        total+= arr.at(t) + arr.at(t-1);
         arr.push_back(total);
-        cout << "back " << arr.back() << endl;
+
     }
-
-
-    cout << arr.back() << endl;*/
-    for(int t=0; t<arr.size(); t++)
+        ans.push_back(arr.back());
+    }
+    for(int t=0; t<ans.size(); t++)
     {
-        cout << arr.at(t) << endl;
+        cout << ans.at(t) << endl;
     }
+
+
+
+
+
 }
