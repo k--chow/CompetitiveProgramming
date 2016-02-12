5fe5860b427ddc80b479e14fe20e9352b75d0e39
kchow23 <kchow23@ufl.edu>
2016-02-12 12:31:18 -0500

    PALIN

--- SPOJ/PALIN.cpp
+++ SPOJ/PALIN.cpp
@@ -32,24 +32,68 @@ string reverseString(string a)
     return newString;
 }
 
-string stringAddition(string &a)
+string stringAddition(string a)
 {
 
-    for(int i=a.length -1; i> -1; i++)
+    for(int i=a.length() -1; i> -1; i--)
     {
-        a[i] += 1 - 48;
-        if (a[i] == ':' || a[i] == ';') && i != 0)
+        int j = (int)a[i] - 48;
+        //cout << j << endl;
+        j++;
+        if (j == 10)
         {
-            a[i-1]+=1;
+            a[i] = '0';
+            if (i== 0)
+            {
+                //append
+                a = "1" + a;
+            }
         }
+        else
+        {
+            a[i] = j + '0';
+        }
+
+
     }
+    //cout << a << endl;
+    return a;
 }
 
-string closestPalin(string a)
+bool isPalindrome(string a)
 {
-    if (a.length() == 1)
+    string b,c;
+    if (a.length()/2 == 0)
     {
-        return a;
+        b = a.substr(0, a.length()/2);
+        c = a.substr(a.length()/2, a.length()/2);
+
+    }
+    else
+    {
+        b = a.substr(0, a.length()/2);
+        c = a.substr(a.length()/2 +1, a.length()/2);
+
+    }
+    //cout << b << endl;
+    //cout << c << endl;
+    string d = reverseString(c);
+    //cout << d << endl;
+    if (b == d)
+    {
+        //cout << "true" << endl;
+        return 1;
+    }
+
+    return 0;
+}
+
+string closestPalin(string &a)
+{
+    if (isPalindrome(a))
+    {
+        a = stringAddition(a);
+        cout << a << endl;
     }
     //length is even
     string b, c, ans, mid="";
@@ -76,11 +120,22 @@ string closestPalin(string a)
     }
     else //reverse b is smaller than c
     {
-    //add one starting from mid
+    //add one starting from mid odd
 
     //even concat
+    if (a.length() % 2 == 0)
+    {
+        string d = stringAddition(b);
+        ans = d +reverseString(d);
+    }
+    else
+    {
+        string d = stringAddition(b+mid);
+        string e = reverseString(d);
+        e.erase(0, 1);
+        ans = d + e;
+    }
 
-    //odd +1 == -1
 
     }
 
@@ -92,9 +147,11 @@ string closestPalin(string a)
 
 int main()
 {
-    string a = "807";
-    string b = "8007";
-    a[1] += a[1] - 48 + 11;
+    string a = "808";
+    string b = "80018";
+    string c = "3004";
+    //a[1] += a[1] - 48 + 11;
     //cout << closestPalin(b) << endl;
-    cout << a[1]<< endl;
+    //if is palindrome, add one try again
+    cout << closestPalin(c)<< endl;
 }
