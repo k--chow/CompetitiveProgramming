0163927db79fd3caeca23061d7c288949fc607b1
kchow23 <kchow23@ufl.edu>
2016-02-18 19:45:57 -0500

    UVA10496

---
+++ UVA/UVA10496.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <math.h>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sstream>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+
+    int sizex, sizey, startx, starty;
+    cin >> sizex >> sizey >> startx >> starty;
+    int beepers;
+    cin >> beepers;
+    int loc[beepers][beepers];
+    for(int i=0; i <beepers; i++)
+    {
+
+    }
+
+}
