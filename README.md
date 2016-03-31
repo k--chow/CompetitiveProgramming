6707daf138eec40d6e19ed8f1769122a09434841
kchow23 <kchow23@ufl.edu>
2016-03-31 00:03:30 -0400

    SOLVED UFPT SERGRID

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ My Competitive programming solutions in Java/C++. Moved from Java onto C++.
 3/10/16 - Solved EZDIJKSTRA on SPOJ. Try to implement it with an adjacency list,  priorityqueue, and a graph. Solved HackerRank Dijkstra!
 3/11/16 - Chicago not working in terms of precision. Works but not working completely.
 3/14/16 - Solved CSTREET Spoj with PRIMS.
-
+3/29/16 - Solved SERGRID. BFS and count depth.
--- UFPT/SERGRID.cpp
+++ UFPT/SERGRID.cpp
@@ -16,31 +16,51 @@ typedef pair<int, int> p;
 
 void queueItUp(p current, deque<p>& q, vii & visited, vii graph, int m, int n)
 {
+
     int value = graph[current.first][current.second];
+    //cout << "Current: " << current.first << " " << current.second << " " << value << endl;
 
     //up
-    if (current.second+value < m && visited[current.first][current.second+value] == 0)
+    if (current.first+value < m && visited[current.first+value][current.second] == 0)
     {
-        q.push_back(make_pair(current.first, current.second +value));
+        q.push_back(make_pair(current.first +value, current.second));
+        //cout << current.first+value << " " << current.second<< endl;
+        /*
+        if (dist[current.first][current.second] + 1 < dist[current.first, current.second+value])
+        {
+            //smell trouble but try it, maybe if not less dont queue it
+            dist[current.first][current.second+value] = dist[current.first][current.second] + 1;
+        }*/
     }
 
     //down
-    if (current.second-value > -1 && visited[current.first][current.second-value] == 0)
+    if (current.first-value > -1 && visited[current.first-value][current.second] == 0)
     {
-        q.push_back(make_pair(current.first, current.second + value));
+        q.push_back(make_pair(current.first - value, current.second));
+        // cout << current.first-value << " " << current.second << endl;
+        /*
+        if (dist[current.first][current.second] + 1 < dist[current.first, current.second-value])
+        {
+            //smell trouble but try it, maybe if not less dont queue it
+            dist[current.first][current.second+value] = dist[current.first][current.second] + 1;
+        }*/
     }
 
     //left
-    if (current.first-value > -1 && visited[current.first-value][current.second] == 0)
+    if (current.second-value > -1 && visited[current.first][current.second-value] == 0)
     {
-        q.push_back(make_pair(current.first-value, current.second));
+        q.push_back(make_pair(current.first, current.second-value));
+         //cout << current.first << " " << current.second-value << endl;
     }
 
     //right
-    if (current.first+value < n && visited[current.first+value][current.second] == 0)
+    if (current.second+value < n && visited[current.first][current.second+value] == 0)
     {
-        q.push_back(make_pair(current.first+value, current.second));
+        q.push_back(make_pair(current.first, current.second+value));
+         //cout << current.first << " " << current.second+value << endl;
     }
+
+
 
 }
 
@@ -49,7 +69,9 @@ int main()
     //save 2d array, -1 for parsing
     int m, n;
     cin >> m >> n;
+    //cout << "y " << m << "x " << n << endl;
     vii graph(m);
+    vii visited(m);
     for(int i=0; i<m; i++)
     {
         string s;
@@ -57,53 +79,62 @@ int main()
         for(int j=0; j<n; j++)
         {
             graph[i].push_back((int)s[j] -'0');
-            //cout << (int)s[j] -'0';
-        }
-    }
-    //save 2d array from visited
-    vii visited(m);
-    for(int i=0; i<m; i++)
-    {
-        for(int j=0; j<n; j++)
-        {
             visited[i].push_back(0);
+            //cout << graph[i][j] << " ";
         }
+        //cout << endl;
     }
+    //cout << "part " << graph[2][0] << endl;
+    //save 2d array from visited
+
 
     //start from 0,0 - set to visited
     visited[0][0] = 1;
+    //dist[0][0] = 0;
 
     //do first one add to queue<pair> value of coordinate up right down left
     deque<p> q;
     p a = make_pair(0, 0);
     queueItUp(a, q, visited, graph, m, n);
     bool found = false;
-    int c = 1;
+    int counter = 2; //2 because 1 is 0
+    int scounter = 0;
+    int s = q.size();
+
     while (!q.empty())
     {
-    c++;
+    //cout << "hi" << endl;
     //pop queue as next place to visit. now set to visited
     p next = q.front();
     q.pop_front();
-    visited[next.first][next.second] = 1;
+    scounter++;
+    visited[next.first][next.second] = counter;
+    //visited[next.first][next.second] = 1;
     //add all possible places to queue that are unvisited
     queueItUp(next, q, visited, graph, m, n);
     //terminate if (m, n) is visited
-    if (visited[n-1][m-1] == 1)
+    if (scounter == s)
+    {
+        counter++;
+        scounter = 0;
+        s = q.size();
+    }
+
+    if (visited[m-1][n-1] != 0)
     {
         found = true;
         break;
     }
+
     }
 
     if (found)
     {
-        cout << "FOUND" << endl;
-        cout << c << endl;
+        cout << counter-1 << endl;
     }
     else
     {
-        cout << "NOT FOUND" << endl;
-        cout << c << endl;
+        cout << -1 << endl;
     }
+
 }
