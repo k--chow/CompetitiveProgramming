8f431fa1de1ff3b0b782da2665b15e1d7bceda4b
kchow23 <kchow23@ufl.edu>
2016-04-15 21:35:51 -0400

    Still solving POTHOLE

--- SPOJ/POTHOLE.cpp
+++ SPOJ/POTHOLE.cpp
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <cstdlib>
+#include <queue>
+#include <stack>
 using namespace std;
 
 
@@ -24,7 +26,7 @@ int main() {
     {
         for(int j=0; j<chambers; j++)
         {
-            capacity[i][j] = -1;
+            capacity[i][j] = 0;
         }
 
     }
@@ -48,19 +50,11 @@ int main() {
     //source +1 all 1
     for(int i=0; i<chambers;i++)
     {
-        if (capacity[0][i] != -1)
+        if (capacity[0][i] != 0)
         {
             capacity[0][i] = 1;
         }
     }
-
-    //sink -1 all 1
-    /*
-    for(int i=0; i<chambers; i++)
-    {
-        if (capacity[])
-    }
-    */
 
     for (int i=0; i<chambers;i++)
     {
@@ -69,6 +63,123 @@ int main() {
             cout << capacity[i][j] << " ";
         }
         cout << endl;
+
+    }
+
+
+
+    while (true)
+    {
+
+        //bfs find path to residual graph
+        stack<int> path; //keeps track of path
+        queue<int> q;
+        vector<int> pred(chambers, -1);
+        vector<int> visited(chambers, 0);
+        //implement first neighbors
+        visited[0] = 1; //set first to visited
+        //store predecessor as self
+        pred[0] = -1;
+        for(int i=0; i<chambers; i++)
+        {
+            if (capacity[0][i] != 0) // edge exists
+            {
+                pred[i] = 0;
+                q.push(i);
+            }
+        }
+
+        while(!q.empty())
+        {
+            //pop one from queue
+            int node = q.front();
+            q.pop();
+            //set node to visited
+            visited[node] = 1;
+
+            //queue up neighbors
+            for(int i=0; i<chambers; i++)
+            {
+                if (capacity[node][i] != 0 && visited[i] != 1) //edge exists and unvisited
+                {
+                    q.push(i);
+
+                    //store predecessor
+                    pred[i] = node;
+
+                }
+            }
+
+
+            //if visited already, break
+            if (visited[chambers-1] == 1)
+            {
+                break;
+            }
+        }
+        /*
+        for(int i=0; i<chambers; i++)
+        {
+                cout << pred[i]+1 <<endl;
+        }*/
+
+        //get the path in reverse
+        path.push(chambers-1);
+        int curr = pred[chambers-1];
+        path.push(curr);
+        while (curr != 0)
+        {
+            path.push(pred[curr]);
+            curr = pred[curr];
+        }
+        /*make sure path works
+        while (!path.empty())
+        {
+            cout << path.top() << endl;
+            path.pop();
+        }*/
+
+
+        //find min in path
+        int m = 100000;
+        int s = path.size();
+
+        int from = path.top();
+        vector<int> path2;
+        path.pop();
+        int to;
+        path2.push_back(from);
+        cout << from << " " << to << endl;
+        for(int i=0; i<s-1; i++)
+        {
+            to = path.top();
+            path.pop();
+            path2.push_back(to);
+            //cout << n+1 << endl;
+            if (capacity[from][to] < m) m = capacity[from][to];
+            //cout << from << " " << to << " : " << m << endl;
+            //make residual graph
+            capacity[from][to]-=capacity[from][to];//change this to flow
+            capacity[to][from]+=capacity[from][to];
+            from = to;
+
+
+        }
+
+        for(int i=0; i<path2.size(); i++)
+        {
+            cout << path2[i] << endl;
+        }
+
+
+        //update graph with this min in mind
+
+
+        //path not found so exit
+        if (visited[chambers-1] == 1)
+        {
+            break;
+        }
 
     }
 }
