41e052809a9576e8960fba39617bb09388311b2f
kchow23 <kchow23@ufl.edu>
2016-04-20 17:41:17 -0400

    Solved ACODE

--- SPOJ/ACODE.cpp
+++ SPOJ/ACODE.cpp
@@ -11,63 +11,62 @@ using namespace std;
 
 int main()
 {
-    string i;
-    vector<long long> ans;
     while (1)
     {
-        cin >> i;
-        if (i == "0")
+        //cout << endl;
+        vector<long long> DP;
+        long long ans = -1;
+        DP.push_back(1);
+        string input;
+        cin >> input;
+        if (input == "0")
         {
             break;
         }
-
-    vector<long long> arr;
-    arr.push_back(0);
-    arr.push_back(1);
-    //100 fibonacci
-
-    for(int t=1; t<i.length()+1; t++)//each substring
-    {
-        string c = i.substr(0, t);
-        //cout << c << endl;
-        int total = 0;
-        //int a = c.length()-1;
-        for(int j=t-2; j<c.length()-1; j++)//each pair
+        input = "1" + input;
+        //traverse the input
+        for(int i=1; i<input.length(); i++)
         {
-            //check if pair is < 27
-            string ch = i.substr(j, 2);
-            int check = atoi(i.substr(j, 2).c_str());
-            //cout << "check " << check << endl;
-            //total = j;
-            if (check > 26)
+            int a = input[i] - '0';
+            //cout << "a: " << a << endl;
+            //if not zero, DP[i] = DP[i-1]
+            if (a != 0)
             {
-                total--;
+                DP.push_back(DP[i-1]);
+                //cout << "DP: " << DP[i] << endl;
+                string b = input.substr(i-1, 2);
+            //if this and previous create a pair, DP[i]+=DP[i-2];
+            int c = atoi(b.c_str());
+            if ((c < 27 && c > 0) && i != 1 && input[i-1] != '0')
+            {
+
+                DP[i] += DP[i-2];
             }
-            if (ch == "00")
+            else if (c == 0)
             {
-                //make it all zero
+                ans = 0;
+                break;
+            }
+            }
+            else
+            {
+                DP.push_back(DP[i-2]);
             }
 
-            if (ch[0] == '0' || ch[1] == '0')
-            {
-                //subtract from previous
-                arr.at(t-1)--;
-            }
 
 
+
+
+            //cout << i << " " << DP[i] << " " << a << endl;
 
         }
-        total+= arr.at(t) + arr.at(t-1);
-        arr.push_back(total);
+        if (ans == -1)
+        {
+            ans = DP[input.length()-1];
+        }
+        cout << ans << endl;
 
     }
-        ans.push_back(arr.back());
-    }
-    for(int t=0; t<ans.size(); t++)
-    {
-        cout << ans.at(t) << endl;
-    }
-
 
 
 
