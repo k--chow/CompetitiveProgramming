edaa4e676457584dfe6ec57418c846c5648396da
kchow23 <kchow23@ufl.edu>
2016-04-22 14:58:39 -0400

    Still Working on LCASQ
    Finished levels and euler path arrays

--- SPOJ/LCASQ.cpp
+++ SPOJ/LCASQ.cpp
@@ -13,8 +13,9 @@ using namespace std;
 //write RMQ function to find smallest level in between indexes
 
 //write LCA function to return euler[RMQ(first_occurence[i], first_occurence[j], level)]
+bool finish = false; //finish is used to find out when to end the recursive statements
 
-void DFS (vector<int> & euler, vector<int> & level, vector< vector<int> > graph,  int current, int previous, int n)
+void DFS (vector<int> & euler, vector<int> & level, vector< vector<int> > graph,  int current, int previous, int n, int currentLevel)
 {
     //get all the neighbors
     for(int i=0; i<graph[current].size(); i++)
@@ -22,14 +23,29 @@ void DFS (vector<int> & euler, vector<int> & level, vector< vector<int> > graph,
         //make sure its not parent(previous)
         if (graph[current][i] != previous)
         {
+
             euler.push_back(graph[current][i]);
-            DFS(euler, level, graph, graph[current][i], current, n);
-            if (graph[current][i] == n-1)
+            //cout << "LEVEL: " << currentLevel+1 << endl;
+            level.push_back(currentLevel + 1);
+
+            //cout << "ORIGINAL ADD " << graph[current][i] << endl;
+            if (graph[current][i] == n-1) //find a way to break all recursive statements after this break
             {
+                //cout << "BREAK" << endl;
+                finish = true;
                 return;
             }
-            euler.push_back(current); //come back, but not if its the end
-            //cout << current << " " << previous << endl;
+            DFS(euler, level, graph, graph[current][i], current, n, currentLevel+1);
+
+            if (!finish)
+            {
+                //cout << "ADDED " << current << endl;
+                euler.push_back(current); //come back, but not if its the end
+                //cout << "LEVELBACK: " << currentLevel << endl;
+                level.push_back(currentLevel);
+                //cout << current << " " << previous << endl;
+            }
+
 
         }
 
@@ -59,16 +75,21 @@ int main()
 
 
     vector<int> euler; //save path of DFS
-    vector<int> level(2*n + 1, 0); //save level of path of DFS
+    vector<int> level; //save level of path of DFS
 
     //run DFS on graph and save euler and level
     //visited array not needed
-    euler.push_back(0);
-    DFS(euler, level, graph, 0, 0, n);
+    euler.push_back(0); //add root to euler array
+    level.push_back(0); //add root level to level array
+
+    DFS(euler, level, graph, 0, 0, n, 0);
+
     for(int i=0; i<euler.size(); i++)
     {
-        cout << euler[i] << endl;
+        cout << euler[i] << " " << level[i] << endl;
     }
+
+
 
 
 
