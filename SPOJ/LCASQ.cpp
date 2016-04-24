9692937b9b859cd15dc6d99ae64795ef551ace56
kchow23 <kchow23@ufl.edu>
2016-04-24 12:54:22 -0400

    LCASQ still TLE

--- SPOJ/LCASQ.cpp
+++ SPOJ/LCASQ.cpp
@@ -11,14 +11,18 @@ using namespace std;
 
 //segment tree solely for Range Minimum Queries (RMQ) of levels
 class SegmentTree {
+
+    public:
     vector<int> levels; //backing array
     vector<int> left; //store left nodes array
     vector<int> right; //store right nodes array
     vector<int> tree; //stores index of minimum level
     int s; //size;
+    int m; //min
 
     SegmentTree(vector<int> level)
     {
+        this->m = 1000000;
         this->levels = level;
         this->s = level.size();
         for(int i=0; i<s*4; i++)
@@ -28,40 +32,102 @@ class SegmentTree {
             right.push_back(0);
         }
         //now build
+        build(0, 0, s-1);
     }
 
     void build(int tree_index, int start_index, int end_index) //build the tree
     {
+        left[tree_index] = start_index;
+        right[tree_index] = end_index;
         //if start = end, set that tree_index to itself, then break
-        if (left[tree_index] == right[tree_index])
+        if (start_index == end_index)
         {
-            this->tree[tree_index] = levels[start_index];
+            this->tree[tree_index] = start_index;
             return;
         }
         //find smallest level in between the 2 indexes
+        int smallest = 1000000;
+        int index;
+        for(int i=start_index; i<end_index+1; i++)
+        {
+            if (this->levels[i] < smallest)
+            {
+                index = i;
+                smallest = this->levels[i];
+            }
+        }
 
         //set tree[tree_index] to that value
-
+        tree[tree_index] = index;
+        //cout << tree[tree_index] << endl;
         //build left node
+        //cout << "Building left " << start_index << " " << (start_index + end_index)/2 << endl;
+        build((2*tree_index) + 1, start_index, (start_index + end_index)/2);
 
+        //cout << "Building right " << ((start_index + end_index)/2)+1 << " " << end_index << endl;
         //build right node
+        build((2*tree_index) + 2, ((start_index + end_index)/2)+1, end_index);
     }
-    //return index of smallest level in between the indexes first_occurrence[i] and first_occurence[j]
-    /*
-    int RMQ(int node_i, int node_j)//returns index of smallest level in between nodes i and j
+
+    //query the segment/fenwick tree sum
+    //solutions - save pairs of the index, use left/right for determining
+    void c_min_query(int i, int j, int index) // add to a running min
     {
 
-    }*/
+    	if (index == 0 && this->m != 1000000)//reset min from other queries
+    	{
+    	    //cout << "reset" << endl;
+    		this->m = 1000000;
+    	}
+        //use range and check
+        //orange - parts of it in it but parts of it not - continue on
+        //traverse again
+
+        //green - all a part of it, math.min this on this
+        if (left[index] >= i && right[index] <= j)
+        {
+            if (this->m == 1000000)
+            {
+                this->m = tree[index];//set things to the index, not the actual level
+            }
+            else
+            {
+                if (levels[tree[index]] < levels[this->m])
+                {
+                    this->m = index;
+                }
+            }
+            //cout << "Index: " << tree[index] << " Level " << levels[tree[index]] << endl;
+
+
+            return;
+        }
+        //red all are NOT a part of this, return 0 on this
+        else if ((left[index] > j) || (right[index] < i))
+        {
+
+            return;
+        }
+        else //orange node
+        {
+
+            c_min_query(i, j, (2 * index) + 1); //left
+            c_min_query(i, j, (2 * index) + 2); //right
+        }
+
+    }
+
 
 
 };
-//write RMQ function to find smallest level in between indexes
+
 
 //write LCA function to return euler[RMQ(first_occurence[i], first_occurence[j], level vector, first_occurence vector)]
 bool finish = false; //finish is used to find out when to end the recursive statements
 
 
-void DFS (vector<int> & euler, vector<int> & level, vector< vector<int> > graph,  int current, int previous, int n, int currentLevel)
+
+void DFS (vector<int> & euler, vector<int> & level, vector< vector<int> > graph,  int current, int previous, int n, int currentLevel, vector<int> & first_occurence)
 {
     //get all the neighbors
     for(int i=0; i<graph[current].size(); i++)
@@ -71,8 +137,13 @@ void DFS (vector<int> & euler, vector<int> & level, vector< vector<int> > graph,
         {
 
             euler.push_back(graph[current][i]);
+            if (first_occurence[graph[current][i]] == -1)
+            {
+                first_occurence[graph[current][i]] = euler.size()-1 ; // set first_occurence
+            }
             //cout << "LEVEL: " << currentLevel+1 << endl;
             level.push_back(currentLevel + 1);
+
 
             //cout << "ORIGINAL ADD " << graph[current][i] << endl;
             if (graph[current][i] == n-1) //find a way to break all recursive statements after this break
@@ -81,7 +152,7 @@ void DFS (vector<int> & euler, vector<int> & level, vector< vector<int> > graph,
                 finish = true;
                 return;
             }
-            DFS(euler, level, graph, graph[current][i], current, n, currentLevel+1);
+            DFS(euler, level, graph, graph[current][i], current, n, currentLevel+1, first_occurence);
 
             if (!finish)
             {
@@ -122,35 +193,17 @@ int main()
 
     vector<int> euler; //save path of DFS
     vector<int> level; //save level of path of DFS
+    vector<int> first_occurence(n, -1); //save first occurence of nodes
+
 
     //run DFS on graph and save euler and level
     //visited array not needed
     euler.push_back(0); //add root to euler array
     level.push_back(0); //add root level to level array
+    first_occurence[0] = 0; // implementation detailð©ó `cU`cUÐy^
+    DFS(euler, level, graph, 0, 0, n, 0, first_occurence);
 
-    DFS(euler, level, graph, 0, 0, n, 0);
-    /*
-    for(int i=0; i<euler.size(); i++)
-    {
-        cout << euler[i] << " " << level[i] << endl;
-    }*/
-
-    //precompute first_occurence
-    vector<int> first_occurence; //save first occurence of nodes
-    for(int i=0; i<n; i++)// for each node i
-    {
-        //find f[i];
-        for(int j=0; j<euler.size(); j++)
-        {
-            if (euler[j] == i)
-            {
-                first_occurence.push_back(j);
-                //cout << j << endl;
-                break;
-            }
-        }
-
-    }
+    SegmentTree test(level); //initialize segment tree
 
     int queries;
     cin >> queries;
@@ -169,8 +222,10 @@ int main()
             index_i = a;
             index_j = b;
         }
-        //check if index needs to be reversed
-        cout << euler[RMQ(index_i, index_j, level, first_occurence)] << endl;
+        //check if index needs to be reversed, error will occur if first is larger than second
+
+        test.c_min_query(first_occurence[index_i], first_occurence[index_j], 0);
+        cout << euler[test.m] << endl;
     }
 
 
