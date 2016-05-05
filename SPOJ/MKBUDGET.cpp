cad4f1cb9e775935dd65fc9f8823285466fd6c67
kchow23 <kchow23@ufl.edu>
2016-05-05 10:34:52 -0400

    MKBUDGET basically solved
    small bug in DP solve method

--- SPOJ/MKBUDGET.cpp
+++ SPOJ/MKBUDGET.cpp
@@ -11,29 +11,21 @@
 
 using namespace std;
 
-void solve(vector< vector<int> > & DP, int needed, int i, int j, int newHire, int sever, int current)
+void solve(vector< vector<int> > & DP, int needed, int i, int j, int newHire, int sever, int current, int ending)
 {
-    int ending;
-    //first find index of where to end
-    for(int a=0; a<15; a++)
-    {
-        if (DP[i-1][a] != -1) ending = a;
-    }
 
-    cout << ending << endl;
-    for(int c= j; c<ending; c++)
+    //needed case
+    if (j == needed)
     {
-        if (c == needed) //max it out
-        {
-            int mini = 100000;
-            //find min of (row above + getting to current state)
-            for(int b=0; b<15; b++)
+    int mini = 100000;
+    //find min of (row above + getting to current state)
+            for(int b=0; b<30; b++)
             {
                 if (DP[i-1][b] != -1 )
                 {
-                    int above;
-                    int index = b;
-                    if (index < needed)
+                    int above; //always adds up to the index j
+                    int index = b; //current j column
+                    if (index < needed)//keep current employees + add new ones
                     {
                         above = (needed-index)*newHire + (index*current);
                     }
@@ -43,109 +35,131 @@ void solve(vector< vector<int> > & DP, int needed, int i, int j, int newHire, in
                     }
                     else //index > needed
                     {
-                        above = (needed*current) + (index - needed)*sever;
+                        above = (needed*current) + ((index - needed)*sever);
                     }
-
-                    if ((DP[i-1][b] + above) < mini)
+                    int total = DP[i-1][b] + above;
+                    //cout << DP[i-1][b] << " " << above << endl;
+                    if (total < mini)
                     {
-                        mini = DP[i-1][b] + above;
+                        mini = total;
                     }
 
                 }
 
 
             }
-            DP[i][c] = mini;
+            DP[i][j] = mini;
+    }
+    //loop the other cases
+    else if (j <= ending && j > needed)
+    {
 
-
-
-
-
-        }
-        else //just bring it down No, also more on right
-        {
-
+    for(int c = j; c<=ending; c++)
+    {
             int mini = 100000;
-            //find min of (row above + getting to current state)
-            for(int b=c; b<ending; b++)
+            //find min of (row above and across+ getting to current state) from above
+            for(int b=c; b<=ending; b++)
             {
-                cout << "ok" << endl;
                 if (DP[i-1][b] != -1 )
                 {
                     int above;
-                    int index = b;
-                    if (index < needed)
+
+                    if (b < c)
                     {
-                        above = (needed-index)*newHire + (index*current);
+                        above = (c-b)*newHire + (b*current);
                     }
-                    else if (index == needed)
+                    else if (b == c)
                     {
-                        above = index*current;
+                        above = b*current;
+                        //cout << "above: " << above;
                     }
-                    else //index > needed
+                    else //b > c
                     {
-                        above = (needed*current) + (index - needed)*sever;
+                        above = (b * current) + (b - c) * sever; //what about other indexes?
+                    }
+                    int total = (DP[i-1][b] + above);
+                    if (total < mini)
+                    {
+                        mini = total;
                     }
 
-                    if ((DP[i-1][b] + above) < mini)
-                    {
-                        mini = DP[i-1][b] + above;
-                    }
+                    //cout << "above: " << (needed*current) + (index - needed)*sever << " " << b <<endl;
 
                 }
             }
             DP[i][c] = mini;
 
 
+
     }
-}
+    }
 }
 
 int main()
 {
-    vector< vector<int> > DP(30);
-    for(int i=0; i<30; i++)
+    while (1)
     {
-        for(int j=0; j<30; j++)
+        vector< vector<int> > DP(30);
+        for(int i=0; i<30; i++)
         {
-            DP[i].push_back(-1);
-        }
-    }
-    int hire = 400, current = 600, sever = 600;
-    int newHire = hire+current;
-    vector<int> n;
-    n.push_back(11);
-    n.push_back(9);
-    n.push_back(10);
-    n.push_back(14);
-    n.push_back(9);
-    n.push_back(9);
-    n.push_back(13);
-    n.push_back(15);
-    DP[0][11] = newHire*11;
-    for(int i=1; i<2; i++)
-    {
-        for(int j=0; j<15; j++)
-        {
-            if (j < n[i]) //don't do anything
+            for(int j=0; j<30; j++)
             {
+                DP[i].push_back(-1);
+            }
+        }
+        int months;
+        cin >> months;
+        if (months == 0) break;
+        int hire, current, sever;
+        cin >> hire >> current >> sever;
+        int newHire = hire+current;
+        vector<int> n;
+        for(int i=0; i<months; i++)
+        {
+            int employees_needed;
+            cin >> employees_needed;
+            n.push_back(employees_needed);
+        }
+        DP[0][n[0]] = newHire*n[0]; //implementation detail
+        for(int i=1; i<months; i++)
+        {
+            int ending;
+            //first find index of where to end (from previous)
+            for(int a=0; a<30; a++)
+            {
+                if (DP[i-1][a] != -1) ending = a;
+            }
+            //now it begins, now it ends
+            for(int j=n[i]; j<30; j++) //j starts at needed
+            {
+                    solve(DP, n[i], i, j, newHire, sever, current, ending);
 
             }
-            else
-            {
-                solve(DP, n[i], i, j, newHire, sever, current);
-            }
-
         }
-    }
 
-    for(int i=0; i<8; i++)
-    {
-        for(int j=0; j<15; j++)
+        //cout << DP[months-1][n[months-1]];
+
+        for(int i=0; i<months; i++)
         {
-            cout << DP[i][j] << " ";
+            //now it begins, now it ends
+            for(int j=0; j<30; j++) //j starts at needed
+            {
+                    cout << DP[i][j] << " ";
+
+            }
+            cout << endl;
         }
-        cout << endl;
+
     }
+
+
+
+
+
+
+
+
+
+
 
 }
