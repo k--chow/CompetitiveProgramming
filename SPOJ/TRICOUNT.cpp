9cbf9847e8fbd0de9c3a34e81843550b3c616c5f
kchow23 <kchow23@ufl.edu>
2016-05-20 07:24:23 -0400

    Finally solved TRICOUNT

--- SPOJ/TRICOUNT.cpp
+++ SPOJ/TRICOUNT.cpp
@@ -1,1 +1,20 @@
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+using namespace std;
+int main()
+{
+    int d;
+    cin >> d;
+    for(int i=0; i<d; i++)
+    {
+        long long n;cin >> n;long long m = n/2;long long ans = 0;long long a = ((n*(n+1)*(n+2))/6) + (m*(m+1)*((2*m)+1))/3;long long c = ((m*m) + m)/2;if (n % 2 == 0) ans = a-c;if (n % 2 != 0) ans = a+c;cout << ans << endl;
+    }
+}
+
+//500B limit is dumb
