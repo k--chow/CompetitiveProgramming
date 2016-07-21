2eae4aa48f7aba06401addb47a4a0216e0fd8a5d
kchow23 <kchow23@ufl.edu>
2016-07-21 08:01:38 -0400

    HOTELS

--- SPOJ/HOTELS.cpp
+++ SPOJ/HOTELS.cpp
@@ -12,31 +12,17 @@ using namespace std;
 //must be consecutive, if previous taken, must take
 
 //recursive backtracking
-int money[300000];
+long long money[300000];
+//long long currentMoney[300000];
 int goal;
 int N;
-long long buy(int currentChoice, long long currentMoney)//is previous bought?
-{
-    if (currentMoney > goal) { // can't buy
-        //backtrack??
-        return currentMoney - money[currentChoice -1];
-    }
-    if (currentChoice == N)
-    {
-        return currentMoney;
-    }
-    //cout << currentMoney << endl;
-    if (currentMoney == goal) return currentMoney; // reached
 
+//if currentMoney[currentChoice-1] + money[currentChoice] > goal - return 0;
+//if currentChoice == N return;
+//if currentMoney[currentChoice-1] + money[currentChoice] - return self
+//currentMoney[] 
 
-    return max(buy(currentChoice+1, 0), buy(currentChoice+1, currentMoney + money[currentChoice]));//is this why it's not consecutive? yes.
-    //if you skip, back to 0
-
-
-
-
-
-}
+//4 5 1 0 3 1
 
 int main()
 {
@@ -48,8 +34,21 @@ int main()
         cin >> value;
         money[i] = value;
     }
-    //for loop to buy max
-    cout << buy(0, 0)<< endl;
+    //currentMoney[0] = 0;
+    //cout << buy(0)<< endl;
+    long long ans = 0;
+    long long current = 0;
+    for(int i=0; i<N; i++)
+    {
+        current += money[i];
+        if (current > goal)
+        {
+            ans = max(ans, current -= money[i]);
+            current = 0;
+        }
+    }
+
+    cout << ans << endl;
 }
 
 
