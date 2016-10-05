656f1bb5568f26fc5d51adf1bdfeed87bd1e59a5
kchow23 <kchow23@ufl.edu>
2016-10-04 23:58:52 -0400

    359

---
+++ LEETCODE/359.cpp
@@ -0,0 +1,25 @@
+class Logger {
+public:
+    /** Initialize your data structure here. */
+    map<string, int> log;
+    Logger() {
+
+    }
+
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    bool shouldPrintMessage(int timestamp, string message) {
+        if (log.count(message) == 0 || (timestamp - log[message]) > 9 ) {
+            log[message] = timestamp;
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * bool param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
