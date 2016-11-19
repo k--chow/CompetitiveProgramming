ee62d38f26c504a284022e3169cb67d216e84ec5
kchow23 <kchow23@ufl.edu>
2016-11-18 23:47:21 -0500

    Cookies

---
+++ LEETCODE/455AssignCookies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int i=0, j=0;
+        int ans = 0;
+        while (i != g.size() && j != s.size())
+        {
+            if (g[i] > s[j]) {
+                j++;
+            } else {
+                i++, j++, ans++;
+            }
+        }
+        return i;
+    }
+};
